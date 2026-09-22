b141f9c826d73995c3ece318bb1a985f3ebece88
ayan-1295 <ssgxayan@gmail.com>
2026-09-22 15:10:17 +0530

    Add files via upload

---
+++ p1.py
@@ -0,0 +1,32 @@
+print("welcome to hyderabad")
+
+print(34)
+
+print("84")
+
+# mention alphabets in double codes, but no. can be mentioned with or without double code
+print(98.25)
+print("python is case sensi")
+
+ print statement
+
+ python is case sensitive
+
+---->comments<-----
+
+-----> comments aren't executed by python <------
+
+types of comments
+
+1.single line cmmnts
+2.multiline cmmnt
+
+single line cmmnt is denoted by #
+multiple line cmmnt is denoted by triple quotes
+
+variable
+#variable is a container that stores value
+for name : string
+for no. : int
+for decimal : float
+ 
